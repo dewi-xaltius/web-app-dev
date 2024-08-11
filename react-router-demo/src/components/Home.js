0fb3b12069ec58bcb99ab5c19d495dfea7293b79
dewi-xaltius <smdewi@xaltius.tech>
2024-08-11 09:56:09 +0800

    add simple styling for Home.js using Home.module.css

--- react-router-demo/src/components/Home.js
+++ react-router-demo/src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import styles from './styles/Home.module.css';
+
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return <div className={styles.home}>Home Page</div>;
 };
 
 export default Home;
